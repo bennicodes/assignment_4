2971cdc45f0162f7ce8e55ab46bd0f5e952a5b42
Benedict Notoane <benni@MacBookPro.lan>
2024-12-02 02:48:56 +0100

    added tabs component

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,27 @@
+const body = document.querySelector("body");
 const navbarButtons = document.querySelectorAll(".navbar__button");
 const componentContainers = document.querySelectorAll(".component__container");
-const instructionText = document.querySelector(".instruction__text");
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Create the instruction message
+  const instruction = document.createElement("p");
+  instruction.textContent = "Press the menu button to select a component";
+  instruction.classList.add("instruction__text");
+
+  body.append(instruction);
+
+  // Remove instruction
+  function clearInstruction() {
+    if (instruction) {
+      instruction.remove();
+    }
+  }
+
+  // Removes instruction after 10 seconds
+  setTimeout(() => {
+    clearInstruction();
+  }, 5000);
+});
 
 // Component selection ------------------
 navbarButtons.forEach((button) => {
