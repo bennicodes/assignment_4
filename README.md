1f5e3f6832d255c4d38ed7507efd0fdeec8dbb11
Benedict Notoane <benni@Benedicts-MacBook-Pro.local>
2024-12-04 09:57:00 +0100

    added functionality to tooltip and README.md

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Web Components
 
+## Description
+
+This assignment contains a list of components which are in different states.
+
+I chose these components because I feel like almost all of them are used in every functional website you can find and therefore highly relevant to becoming a good developer.
+
 ## Sources
 
 https://www.youtube.com/watch?v=UQKWc2r_41U&ab_channel=Coding2GO
 
-I used this video as inspiration for some styling, transitions and creating the tooltip component.
+I used this video as inspiration for some styling, transitions when creating the tooltip component. The person in the video only used css. So i made the functionality of the tooltip component in javascript to shorten the code length.
+
+The accordion-, tabs-, and search filter component are from a workshop which we did in class.
--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -1,2 +1,26 @@
-const facebookTooltip = document.querySelector(".facebook");
+const socialContainer = document.querySelector(
+  ".social__container--functional"
+);
+const facebookLink = document.querySelector(".facebook");
+const tooltipBox = document.querySelector(".tooltip__box--functional");
+tooltipArrow = document.querySelector(".tooltip__arrow--functional");
 
+function showTooltip() {
+  facebookLink.classList.add("facebook--active");
+
+  tooltipBox.classList.add("tooltip__box--active");
+  tooltipBox.textContent = "Facebook";
+
+  tooltipArrow.classList.add("tooltip__arrow--active");
+}
+
+function removeTooltip() {
+  facebookLink.classList.remove("facebook--active");
+  tooltipBox.classList.remove("tooltip__box--active");
+  tooltipArrow.classList.remove("tooltip__arrow--active");
+  tooltipBox.textContent = "";
+}
+
+facebookLink.addEventListener("mouseenter", showTooltip);
+
+facebookLink.addEventListener("mouseleave", removeTooltip);
